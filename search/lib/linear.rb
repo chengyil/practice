88f354f529d269aa90e09ea89940320f780857b4
Cheng Yi Lee <chengyil@sg.ibm.com>
2018-01-17 01:45:20 +0800

    Extract Search for DSL

--- search/lib/linear.rb
+++ search/lib/linear.rb
@@ -1,15 +1,16 @@
 require_relative 'search'
-class Linear < Search
-  class << self
-    def run(item, haystack)
-      found_index = nil
-      haystack.each.with_index(0) do |i,index|
-        if i == item 
-          found_index = index 
-          break
-        end
+class Linear
+  extend Search
+
+  search_implementation do |item, haystack|
+    found_index = nil
+    haystack.each.with_index(0) do |i,index|
+      if i == item 
+        found_index = index 
+        break
       end
-      found_index
     end
+    found_index
   end
+
 end
--- search/lib/search.rb
+++ search/lib/search.rb
@@ -1,12 +1,14 @@
 require 'benchmark'
-class Search
-  class << self
-    def search(item, haystack)
-      found_index = nil
-      time = Benchmark.measure do
-        found_index = run(item, haystack)
-      end
-      found_index
+module Search
+  def search_implementation(&block)
+    @search_implementation = block
+  end
+
+  def search(item, haystack)
+    found_index = nil
+    time = Benchmark.measure do
+      found_index = @search_implementation.call(item,haystack)
     end
+    found_index
   end
 end
